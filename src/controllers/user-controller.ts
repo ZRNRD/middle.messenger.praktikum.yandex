e794184b70fc2d347c1bff6cf46f49a7dcf281f4
Владимир Филимоненко <zrnrd@mail.ru>
2022-11-13 20:49:47 +0500

    added user-controller.ts

---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,51 @@
+import { ISignUpData, IPasswordData } from '../utils/interfaces';
+import { UserApi } from '../api/user-api';
+import { store } from '../store/store';
+import { AuthController } from './auth-controller';
+
+const userInstance = new UserApi();
+const loginController = new AuthController();
+
+export class UserController {
+  public async changeUserProfile(data: ISignUpData) {
+    try {
+      await userInstance.changeUserProfile(data);
+      await loginController.getUser();
+    } catch (e) {
+      return e.reason;
+    }
+  }
+
+  public async changeUserAvatar(file: File, image: HTMLImageElement) {
+    try {
+      const data = new FormData();
+      data.append('avatar', file, `avatar.${file.type.split('/')[1]}`);
+      await userInstance.changeUserAvatar(data);
+      this.updateImage(file, image);
+      await loginController.getUser();
+    } catch (e) {
+      return e.reason;
+    }
+  }
+
+  public updateImage(file: File, image: HTMLImageElement) {
+    const reader = new FileReader();
+
+    reader.onload = async (ev) => {
+      const base64 = ev.target.result;
+      image.src = base64 as string;
+      store.setStateAndPersist({ avatarIcon: base64 }, true);
+    };
+
+    reader.readAsDataURL(file);
+  }
+
+  public async changeUserPassword(data: IPasswordData) {
+    try {
+      await userInstance.changeUserPassword(data);
+      await loginController.getUser();
+    } catch (e) {
+      return e.reason;
+    }
+  }
+}
