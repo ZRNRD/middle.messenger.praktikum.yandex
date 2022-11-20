99cce0bfceafad7106d21ff4673d0c135c715ee2
Владимир Филимоненко <zrnrd@mail.ru>
2022-11-20 15:29:38 +0500

    added mocha and chai tests

---
+++ src/components/button/button.spec.ts
@@ -0,0 +1,17 @@
+import { expect } from 'chai';
+import { Button } from './button';
+import { TButton } from '../../utils/types';
+
+describe('Проверяем компоненту Button', () => {
+  const createButton = (context: TButton) => {
+    const btn = new Button(context);
+    return btn.transformToString();
+  };
+
+  it('Проверяем отрисовку компонента', () => {
+    const context = { title: 'Отмена', className: 'back-chat-button' };
+    const btn = createButton(context);
+    // eslint-disable-next-line no-unused-expressions
+    expect(btn).to.not.be.null;
+  });
+});
